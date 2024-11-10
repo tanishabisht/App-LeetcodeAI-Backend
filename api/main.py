33137afa8708e724c24be1877d6f36e352d620ae
Tanisha Bisht <agamyatani@gmail.com>
2024-11-10 02:29:51 -0500

    add vercel

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # middleware - allow CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # adjust to the frontend's URL
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
